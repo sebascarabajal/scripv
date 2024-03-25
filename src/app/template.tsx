1f280c4b824d2b55537ffa134fba120c3667eab5
sebascarabajal <sebasfmt@gmail.com>
2024-03-25 08:45:23 -0300

    add animation

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -7,7 +7,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      transition={{ ease: 'easeInOut', duration: 0.75 }}
+      transition={{ ease: 'easeInOut', duration: 0.95 }}
     >
       {children}
     </motion.div>
