6d5bc7e755b6553f21dfce9e0bc87481f019d7c6
sebascarabajal <sebasfmt@gmail.com>
2024-03-27 16:34:17 -0300

    auth terminado

--- src/components/auth/SigninForm.tsx
+++ src/components/auth/SigninForm.tsx
@@ -3,82 +3,100 @@ import { EnterIcon, EnvelopeClosedIcon, LightningBoltIcon, LockClosedIcon } from
 import { Button, Flex, Text, TextFieldInput, TextFieldRoot, TextFieldSlot } from '@radix-ui/themes'
 import React, { useTransition } from 'react'
 import { useForm, Controller } from 'react-hook-form'
-import {signIn} from 'next-auth/react'
-import {useRouter} from 'next/navigation'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+import { ToastContainer, toast } from 'react-toastify'
 
 function SigninForm() {
 
-    const {control, setError, handleSubmit, formState:{errors}} = useForm({values:{email:"", password:""}});
+    const { control, setError, handleSubmit, formState: { errors } } = useForm({ values: { email: "", password: "" } });
 
     const router = useRouter()
 
     const [isPending, startTransition] = useTransition()
 
     const onSubmit = handleSubmit(async (data) => {
-        startTransition(async ()=>{
+        startTransition(async () => {
             const res = await signIn("credentials", {
                 redirect: false,
                 email: data.email,
                 password: data.password
             });
-            if(!res?.ok){
+            if (res?.error) {
+                setError("password", { message: "¡Credenciales inválidas!", type: "manual" })
+                return;
+            }
+            if (!res?.ok) {
                 console.log(res)
             }
-            else{
-                const {error} = res
+            else {
+                const { error } = res
             }
 
+            const notify = () => toast.success("Hello coders it was easy!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+            notify();
             router.push("/dashboard");
-            router.refresh()
+            router.refresh();
         })
     });
 
-  return (
-    <form onSubmit={onSubmit}>
-        <Flex direction="column" gap="3">
-            <label htmlFor='email'>Email</label>
-            <TextFieldRoot>
-                <TextFieldSlot>
-                    <EnvelopeClosedIcon height="16" width="16"></EnvelopeClosedIcon>
-                </TextFieldSlot>
-                <Controller 
-                    name='email' 
-                    control={control} 
-                    rules={{required:{message:"¡Se requiere email!", value: true}}} 
-                    render={({field}) =>{
-                        return(
-                            <TextFieldInput type='email' placeholder='miCorreo@gmail.com' autoFocus {...field}></TextFieldInput>
-                        );
-                    }} 
-                />
-            </TextFieldRoot>
+    return (
+        <>
+            <ToastContainer />
+            <form onSubmit={onSubmit}>
+                <Flex direction="column" gap="3">
+                    <label htmlFor='email'>Email</label>
+                    <TextFieldRoot>
+                        <TextFieldSlot>
+                            <EnvelopeClosedIcon height="16" width="16"></EnvelopeClosedIcon>
+                        </TextFieldSlot>
+                        <Controller
+                            name='email'
+                            control={control}
+                            rules={{ required: { message: "¡Se requiere email!", value: true } }}
+                            render={({ field }) => {
+                                return (
+                                    <TextFieldInput type='email' placeholder='miCorreo@gmail.com' autoFocus {...field}></TextFieldInput>
+                                );
+                            }}
+                        />
+                    </TextFieldRoot>
 
-            {errors.email && <Text className='text-red-400'>{errors.email.message}</Text>}
+                    {errors.email && <Text className='text-red-400'>{errors.email.message}</Text>}
 
-            <label htmlFor='password'>Contraseña</label>
-            <TextFieldRoot>
-                <TextFieldSlot>
-                    <LockClosedIcon height="16" width="16"></LockClosedIcon>
-                </TextFieldSlot>
-                <Controller 
-                    name='password' 
-                    control={control} 
-                    rules={{required:{message:"¡Se requiere contraseña!", value: true}, minLength:{message:"¡La contraseña debe ser de 6 carácteres o más!", value:6}}}
-                    render={({field}) =>{
-                        return(
-                            <TextFieldInput type='password' placeholder='*********' {...field}></TextFieldInput>
-                        );
-                    }} 
-                />
-            </TextFieldRoot>
-            {errors.password && <Text className='text-red-400'>{errors.password.message}</Text>}
+                    <label htmlFor='password'>Contraseña</label>
+                    <TextFieldRoot>
+                        <TextFieldSlot>
+                            <LockClosedIcon height="16" width="16"></LockClosedIcon>
+                        </TextFieldSlot>
+                        <Controller
+                            name='password'
+                            control={control}
+                            rules={{ required: { message: "¡Se requiere contraseña!", value: true } }}
+                            render={({ field }) => {
+                                return (
+                                    <TextFieldInput type='password' placeholder='*********' {...field}></TextFieldInput>
+                                );
+                            }}
+                        />
+                    </TextFieldRoot>
+                    {errors.password && <Text className='text-red-400'>{errors.password.message}</Text>}
 
-            <Button type='submit' mt="4">
-                <EnterIcon height="16" width="16"></EnterIcon>
-                {isPending ? "Cargando..." : "¡Entrar!"}
-            </Button>
-        </Flex>
-    </form>
+                    <Button type='submit' mt="4">
+                        <EnterIcon height="16" width="16"></EnterIcon>
+                        {isPending ? "Cargando..." : "¡Entrar!"}
+                    </Button>
+                </Flex>
+            </form>
+        </>
     )
 }
 
