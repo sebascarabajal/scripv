c7a1f96dfe50278b710a07d9c0277d2b2c9d1a04
sebascarabajal <sebasfmt@gmail.com>
2024-05-20 10:40:52 -0300

    add input select ok

--- src/components/InputRadix.tsx
+++ src/components/InputRadix.tsx
@@ -1,41 +1,50 @@
-import { Select } from "@radix-ui/themes";
+import React from 'react';
+import { Control, Controller, FieldValues, RegisterOptions } from 'react-hook-form';
 
-interface InputProps {
-    value: any;
+interface InputRadixProps {
+  name: string;
+  label: string;
+  control: Control<FieldValues>;
+  rules?: RegisterOptions;
+  value?: any;
 }
 
-const CustomSelect: React.FC<InputProps> = ({value}) => {
+const InputRadix: React.FC<InputRadixProps> = ({ name, label, control, rules, value }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        defaultValue={value}
+        render={({ field }) => (
+          <select {...field}>
+            <option value="486">600 VIV. LOMAS DE TAFI</option>
+            <option value="618">LOMAS SECTOR VI 287 VIV.</option>
+            <option value="619">LOMAS SECTOR VII 294 VIV.</option>
+            <option value="620">LOMAS SECTOR VIII 346 VIV.</option>
+            <option value="621">LOMAS SECTOR IX 282 VIV.</option>
+            <option value="622">LOMAS SECTOR X 284 VIV.</option>
+            <option value="623">LOMAS SECTOR XI 290 VIV.</option>
+            <option value="624">LOMAS SECTOR XII 289 VIV.</option>
+            <option value="625">LOMAS SECTOR XIII 287 VIV.</option>
+            <option value="626">LOMAS SECTOR XIV 312 VIV.</option>
+            <option value="627">LOMAS SECTOR XV 329 VIV.</option>
+            <option value="628">LOMAS SECTOR XVI 178 VIV.</option>
+            <option value="627">LOMAS SECTOR XV 329 VIV.</option>
+            <option value="628">LOMAS SECTOR XVI 178 VIV.</option>
+            <option value="629">LOMAS SECTOR XVII 261 VIV.</option>
+            <option value="630">LOMAS SECTOR XVIII 226 VIV.</option>
+            <option value="631">LOMAS SECTOR XIX 273 VIV.</option>
+            <option value="632">LOMAS SECTOR XX 266 VIV.</option>
+            <option value="692">166 VIV. LOMAS DE TAFI</option>
+            <option value="382">LOS POCITOS 448 VIV-SEC.A/342V  </option>
+          </select>
+        )}
+      />
+    </div>
+  );
+}
 
-    return (
-      <Select.Root defaultValue={value}>
-        <Select.Trigger />
-        <Select.Content>
-          <Select.Group>
-            <Select.Label>Sector</Select.Label>
-            <Select.Item value="orange">600 VIV. LOMAS DE TAFI</Select.Item>
-            <Select.Item value="apple">LOMAS SECTOR VI 287 VIV.</Select.Item>
-            <Select.Item value="opcion3">LOMAS SECTOR VII 294 VIV.</Select.Item>
-            <Select.Item value="opcion4">LOMAS SECTOR VIII 346 VIV.</Select.Item>
-            <Select.Item value="opcion5">LOMAS SECTOR IX 282 VIV.</Select.Item>
-            <Select.Item value="opcion6">LOMAS SECTOR X 284 VIV.</Select.Item>
-            <Select.Item value="opcion7">LOMAS SECTOR XI 290 VIV.</Select.Item>
-            <Select.Item value="opcion8">LOMAS SECTOR XII 289 VIV.</Select.Item>
-            <Select.Item value="opcion9">LOMAS SECTOR XIII 287 VIV.</Select.Item>
-            <Select.Item value="opcion10">LOMAS SECTOR XIV 312 VIV.</Select.Item>
-            <Select.Item value="opcion11">LOMAS SECTOR XV 329 VIV.</Select.Item>
-            <Select.Item value="opcion12">LOMAS SECTOR XVI 178 VIV.</Select.Item>
-            <Select.Item value="opcion13">LOMAS SECTOR XV 329 VIV.</Select.Item>
-            <Select.Item value="opcion14">LOMAS SECTOR XVI 178 VIV.</Select.Item>
-            <Select.Item value="opcion15">LOMAS SECTOR XVII 261 VIV.</Select.Item>
-            <Select.Item value="opcion16">LOMAS SECTOR XVIII 226 VIV.</Select.Item>
-            <Select.Item value="opcion17">LOMAS SECTOR XIX 273 VIV.</Select.Item>
-            <Select.Item value="opcion18">LOMAS SECTOR XX 266 VIV.</Select.Item>
-            <Select.Item value="opcion19">166 VIV. LOMAS DE TAFI</Select.Item>
-            <Select.Item value="opcion20">LOS POCITOS 448 VIV-SEC.A/342V</Select.Item>
-          </Select.Group>
-        </Select.Content>
-      </Select.Root>
-    );
-  };
-
-  export default CustomSelect;
+export default InputRadix;
